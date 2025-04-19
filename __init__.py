b99cf227052f344bffd6999c45eaf56b0daaac21
System <dimasoft976@gmail.com>
2025-04-19 19:41:05 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+'''
+For run code on this compiler.
+Example:
+from compiler import *
+
+run_compiler('a = 3; while (a < 5) a = a + 1')
+Output:
+Execution finished
+a: 5
+'''
+from .compiler import *
